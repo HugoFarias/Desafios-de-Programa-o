8a60323db2fc0d52428df1815c0f2f784f4f94f8
Hugo Farias <hugofarias@live.jp>
2015-03-17 09:45:56 -0300

    comentario sobre EOF adicionado

--- Lista 1/matriz_123.cpp
+++ Lista 1/matriz_123.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
 	int n;
-	while(cin >> n){
+	while(cin >> n){ //EOF == Ctrl+D in Unix, apparently Ctrl+Z in Windows
 		for (int i = 0; i < n; i++){
 			for (int j = 0; j < n; j++){
 				if (i+j==n-1) cout << 2;
