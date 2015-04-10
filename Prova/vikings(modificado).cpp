46de2a7a11608d82e0108e95026c6cf18a17199c
Hugo Farias <hugofarias@live.jp>
2015-04-10 15:08:30 -0300

    reducao no codigo

--- Prova/vikings(modificado).cpp
+++ Prova/vikings(modificado).cpp
@@ -1,44 +1,17 @@
 #include <cstdio>
 #include <deque>
+#include <iostream>
 
 using namespace std;
-
-void dq_charge(deque<char>* d){
-	d->push_front('Z');
-	d->push_front('Y');
-	d->push_front('X');
-	d->push_front('W');
-	d->push_front('V');
-	d->push_front('U');
-	d->push_front('T');
-	d->push_front('S');
-	d->push_front('R');
-	d->push_front('Q');
-	d->push_front('P');
-	d->push_front('O');
-	d->push_front('N');
-	d->push_front('M');
-	d->push_front('L');
-	d->push_front('K');
-	d->push_front('J');
-	d->push_front('I');
-	d->push_front('H');
-	d->push_front('G');
-	d->push_front('F');
-	d->push_front('E');
-	d->push_front('D');
-	d->push_front('C');
-	d->push_front('B');
-	d->push_front('A');
-}
 
 int main(){
 	int n, i, ind, inst=1;
 	scanf("%d", &n);
 	while(n!=0){
-		deque<char> alfa(26);
+		deque<char> alfa;
 		char aux;
-		dq_charge(&alfa);
+		for (char i = 'A'; i <= 'Z'; ++i)
+			d->push_back(i);
 		printf("Instancia %d\n", inst);
 		for (i = 0; i < n; i++){
 			scanf("%d", &ind);
